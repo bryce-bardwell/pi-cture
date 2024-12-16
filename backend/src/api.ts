49489e330f3f09d9bd57b1a430dbbca045e4ddcc
bryce-bardwell <bryce.bardwell@sky.uk>
2024-12-16 14:16:41 +0000

    Update project tsconfig and vercel.json

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -4,7 +4,7 @@ import { PIXEL_GRID_HEIGHT, PIXEL_GRID_WIDTH } from './constants';
 import { spawn } from 'child_process';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 
