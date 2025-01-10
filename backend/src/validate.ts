7bd96073b2e4b667621d65e1c64c6d8631c26551
bryce-bardwell <bryce.bardwell@sky.uk>
2025-01-10 13:38:37 +0000

    Readd validation

--- backend/src/validate.ts
+++ backend/src/validate.ts
@@ -2,13 +2,13 @@ import type { PixelGrid } from '../types';
 import { PIXEL_GRID_HEIGHT, PIXEL_GRID_WIDTH } from './constants';
 
 export const validatePixelGrid = (image: PixelGrid): boolean => {
-  // if (image.length !== PIXEL_GRID_HEIGHT) {
-  //   return false;
-  // }
+  if (image.length !== PIXEL_GRID_HEIGHT) {
+    return false;
+  }
 
-  // if (!image.every((row) => row.length === PIXEL_GRID_WIDTH)) {
-  //   return false;
-  // }
+  if (!image.every((row) => row.length === PIXEL_GRID_WIDTH)) {
+    return false;
+  }
 
   return true;
 };
