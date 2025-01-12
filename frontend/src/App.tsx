ae986832a34071a69333e227445b61fd7260b7c8
bryce-bardwell <bryce.bardwell@sky.uk>
2025-01-12 22:32:20 +0000

    Scaffold UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,36 @@
+import {
+  AppWrapper,
+  Pixel,
+  PixelGrid,
+  ShowGridLinesToggle,
+  SubmitButton,
+} from './App.styles';
+
+import { useState } from 'react';
+
+const gridSquares = Array.from({ length: 64 * 64 }, (_, i) => i);
+
 const App = () => {
-  return <h1> Hello from the frontend </h1>;
+  const [showGridLines, setShowGridLines] = useState(true);
+
+  const handleShowGridLinesToggle = () => {
+    setShowGridLines((value) => !value);
+  };
+
+  return (
+    <AppWrapper>
+      <h1>pi-cture</h1>
+      <PixelGrid>
+        {gridSquares.map((square) => (
+          <Pixel $showGridLines={showGridLines} key={square} />
+        ))}
+      </PixelGrid>
+      <SubmitButton>Submit</SubmitButton>
+      <ShowGridLinesToggle onClick={handleShowGridLinesToggle}>
+        Show Grid Lines
+      </ShowGridLinesToggle>
+    </AppWrapper>
+  );
 };
 
 export default App;
